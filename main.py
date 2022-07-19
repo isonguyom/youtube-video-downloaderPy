dceedfb5adc5903accb4314eb4ac14333cacb51c
isonguyom <isonguyom@gmail.com>
2022-07-19 12:48:24 +0100

    replaced codes in main.py with codes from try.py and deleted try.py

--- main.py
+++ main.py
@@ -1,24 +1,45 @@
-# From the  installed Pytube module, import the youtube library
-from pytube import YouTube 
+# Youtube Video Downloader
+from pytube import YouTube
 
-link = "https://www.youtube.com/watch?v=EDZ25anwgjc"
-yt = YouTube(link)
 
-#Title of video
-print("Title: ",yt.title)
-#Number of views of video
-print("Number of views: ",yt.views)
-#Length of the video
-print("Length of video: ",yt.length,"seconds")
-#Rating
-print("Ratings: ",yt.rating)
-#printing all the available streams
-# yt.streams
 
-#Getting the highest resolution possible
-ys = yt.streams.filter(progressive=True).get_highest_resolution()
+print("\nWELCOME! THIS IS A YOUTUBE VIDEO DOWNLOADER!!!")
 
-#Starting download
-print("Downloading...")
-ys.download()
-print("Download completed!!")
+try:
+    # Request of number of youtube videos to be downloader
+    num_of_videos = int(input("How many videos do you want to download? "))
+
+    links = []
+    for i in range(num_of_videos):
+        video_link =  input("Enter youtube video link: ")
+
+        links.append(video_link)
+
+    print(links)
+    print("\n---------------------------------------------------------------------")
+    for i in links:
+        print("Video", links.index(i)+1)
+        yt = YouTube(i)
+
+        # Video Description  
+        print("Title: ",yt.title) #Title of video       
+        print("Length of video: ",yt.length,"seconds") #Length of the video        
+        print("Ratings: ",yt.rating) #Rating
+
+        # Preparing to download youtube video
+        print("Your download will start shortly.")
+        #Getting the highest resolution possible
+        ys = yt.streams.filter(progressive=True).get_highest_resolution()
+
+        # Downloading youtube video
+        print("Downloading...", links.index(i)+1, "/", len(links))
+        ys.download()
+        
+        # Finished downloading youtube
+        print(links.index(i)+1, "of", len(links), "downloaded!!")
+        print("---------------------------------------------------------------------\n")
+
+except:
+    print("You didn't input a legal integer.")
+
+print("\nTHANKS FOR USING OUR YOUTUBE VIDEO DOWNLOADER!!!\n")
